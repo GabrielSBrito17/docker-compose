e27ed1a0ad466ab3f9c6670ceacad9e0dc8ce8dc
GabrielSBrito17 <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>
2022-09-04 11:54:51 -0300

    commit

--- api/main.py
+++ api/main.py
@@ -5,7 +5,7 @@ dbname = 'db'
 user = "postgre"
 password = "postgre"
 sslmode = 'disable'
-port = "5433"
+port = "5432"
 
 conn_string = "host={0} dbname={1} password={2} sslmode={3} port={4} user= {5}".format(host, dbname, password, sslmode, port, user)
 conn = psycopg2.connect(conn_string)
