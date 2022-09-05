85bd7b76ed78cbb0b4727db9298dd380d4c2f3ae
GabrielSBrito17 <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>
2022-09-05 16:07:13 -0300

    commit

--- api/models/cars.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Optional
-from pydantic import BaseModel
-
-class Car(BaseModel):
-    id: str
-    colors: int
-    models: int
-    id_owner: str
--- api/models/owner.py
+++
@@ -1,7 +0,0 @@
-from typing import List, Optional
-from pydantic import BaseModel
-
-class Owner(BaseModel):
-    id: str
-    colors: int
-    models: int
